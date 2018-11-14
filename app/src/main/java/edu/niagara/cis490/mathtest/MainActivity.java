20c51a54ceac8670232a2bf2137f6c44ad5a83ec
Davidodari <davidkibzodari@gmail.com>
2018-11-15 01:29:51 +0300

    Initial commit

---
+++ app/src/main/java/edu/niagara/cis490/mathtest/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.niagara.cis490.mathtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
